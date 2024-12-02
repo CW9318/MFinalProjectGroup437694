dd2d300edf40bb77c217188fc5f3e3aefcaa437d
CW9318 <wang.15544@osu.edu>
2024-12-02 16:22:29 -0500

    Added comments

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,19 +1,23 @@
 class EvaluationsController < ApplicationController
+    # Handling creating a new presentation
     def create
+        # Initializes instance variables 
         @presentation_event = PresentationEvent.find(params[:presentation_event_id])
+        # Creates new evaluation with the params from the private method
         @evaluation = @presentation_event.evaluations.new(evaluation_params)
     
         if @evaluation.save
           flash[:notice] = "Success"
+          # Sends redirect to student view page(refreshes)
           redirect_to student_path
         else
           flash[:alert] = "Error"
           redirect_to student_path
         end
       end
-    
+      # Private method for controller only
       private
-    
+      # Allows params to contain specific things
       def evaluation_params
         params.permit(:score, :comments, :student_name)
       end
--- app/models/presentation_event.rb
+++ app/models/presentation_event.rb
@@ -1,5 +1,5 @@
 class PresentationEvent < ApplicationRecord
-    has_many :evaluations, dependent: :destroy
+  has_many :evaluations, dependent: :destroy
 
   validates :title, :date, :description, :instructor_name, presence: true
 end
